96a84800a9b2f76b88148f9d578bd1ea50f931c9
achintir-projects <achintir77@gmail.com>
2025-06-11 01:39:58 +0400

    🧹 Remove Vercel config, deploying to Render instead

--- api/index.py
+++
@@ -1,9 +0,0 @@
-# Save this as api/index.py
-from app import app
-
-# Vercel handler
-def handler(request):
-    return app(request.environ, lambda status, headers: None)
-
-# For Vercel compatibility
-application = app
